90b652a6a63c3a64e705004080f1bb3ac5d7e2e1
Sonya Nikolova <snikolova27@gmail.com>
2021-11-11 16:45:25 +0200

    add more tasks

---
+++ Week05/task01.js
@@ -0,0 +1,1 @@
+console.log("SUICA2");
---
+++ Week05/task02.js
@@ -0,0 +1,9 @@
+console.log(6, 2, 6.2);
+console.log(6, -2);
+console.log(6 - 2);
+console.log(-2);
+console.log(6- -2);
+console.log(6*2);
+console.log(6/2);
+console.log((6*2) - (6/2));
+console.log((6*2 - 6) /2);
